cd2316f4feca7c1de1dec03af2ef718bb32eacb9
JeymyOrtiz14 <jeymyortiz4@gmail.com>
2022-03-18 16:46:16 -0600

    Primer Commit del proyecto

---
+++ config/conexion.php
@@ -0,0 +1,20 @@
+<?php
+    class Conectar {
+
+        protected $dbh;
+
+        protected function Conexion(){
+            try {
+                $conectar = $this-> dbh = new PDO("mysql:host=52.152.236.67;dbname=g6_20","G6_20", "dAG0h3zR");
+                return $conectar;
+            }catch (exception $e){
+                print "¡Error BD!: ". $e->getMessage(). "<br/>";
+                die();
+            }
+        }
+
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+    }
+?>
